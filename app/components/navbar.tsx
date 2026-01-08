1bc758a08d3f9ca77b3aa1687de507deb0137d29
Yunex-x <yunex.business@gmail.com>
2026-01-08 13:55:32 +0100

    Initial commit from Create Next App

---
+++ app/components/navbar.tsx
@@ -0,0 +1,4 @@
+export default function Navbar() {
+  return (
+    <nav className="justify-between items-center p-4 bg-gray-800 text-white flex">
+        <h1 className="text-2xl font-bold">Optima</h1>
